e124d129a769bb7e484b10c764923bb12165a794
Saqib Alam <98273935+saqibalam-datascience@users.noreply.github.com>
2025-04-19 12:28:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from langchain_community.document_loaders import WebBaseLoader
 
 from chains import get_website_jd, get_llm, resume_extractor, get_template
 from utils import clean_text, pdf_extractor, deepseek_cleaning
+# from utils import deepseek_cleaning
 
 
 def create_streamlit_app():
