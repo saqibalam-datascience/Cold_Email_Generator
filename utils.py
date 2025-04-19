9252fa17134a008bcb13bfd159115294a448d168
Saqib Alam <98273935+saqibalam-datascience@users.noreply.github.com>
2025-04-19 09:56:37 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,16 @@
+import re
+import fitz# PyMuPDF
+
+def clean_text(text):
+    text = re.sub(r"\n(?=\w)", " ", text)
+    text = re.sub(r"\n", "", text)
+    text = re.sub(r"\xa0", "", text)
+    return text
+
+def pdf_extractor(file):
+    # Ensure the uploaded file is read as a byte stream
+    doc = fitz.open(stream=file.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
