1eee7718dafb0a7e3a69141799e5b6066eaa403c
Saqib Alam <98273935+saqibalam-datascience@users.noreply.github.com>
2025-04-19 09:49:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
 from chains import get_website_jd, get_llm, resume_extractor, get_template
-from portfolio import Portfolio
 from utils import clean_text, pdf_extractor
 
 
